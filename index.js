6fc158cfc8e76ee13195c1308f9c04b2a19a2ca6
SSMalik99 <66541410+SSMalik99@users.noreply.github.com>
2021-10-20 23:40:03 +0530

    This is just for the testin of SPA
    This is just for the testin of custom SPA

---
+++ index.js
@@ -0,0 +1,62 @@
+let mainContent = {
+  'navbar' : `<nav class="navbar navbar-expand-lg navbar-light bg-danger">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link fw-bold text-white page-traveler" data-href="home">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link fw-bold text-white page-traveler cursor-pointer"  data-href="about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link fw-bold text-white page-traveler cursor-pointer" data-href="contact">Contact</a>
+      </li>
+    </ul>
+    
+  </div>
+  </nav>`,
+  'home' : `<div class="card bg-dark m-4 p-2 text-white border-white">
+            <div class="card-header bg-dark h3">
+                Custom Travelers
+            </div>
+            <div class="card-body">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse rerum accusamus pariatur accusantium itaque explicabo, tempora delectus odit repellendus voluptatem nulla similique eum qui aspernatur atque eveniet, excepturi quasi perspiciatis suscipit ad ipsa eos. Esse nulla illum placeat, enim, hic id non similique facere dolor, ipsa earum? Nostrum, libero asperiores? Doloribus fugiat neque amet fugit, voluptatem est minima ad quaerat, dignissimos dolores quibusdam explicabo. Dolorum at inventore impedit, consectetur voluptatum aut ullam vel quibusdam tenetur harum dolore hic, asperiores commodi. Sint, consectetur voluptatibus expedita, quibusdam commodi harum, veniam sunt esse blanditiis quia laudantium temporibus reprehenderit hic aspernatur? Nam accusamus temporibus, eum tempore, neque possimus beatae, soluta necessitatibus mollitia aliquid culpa voluptates unde quasi minima sint ipsa quo earum fugiat? Suscipit a amet dolores quae iusto explicabo necessitatibus. Ex distinctio reprehenderit maxime magnam! Ex quia dicta maiores? Quisquam necessitatibus nulla eum optio laborum sint, dolores dolorem earum fugit aut sit, veritatis, doloremque aliquam mollitia nihil alias sed odit. Suscipit, unde in repellendus ipsum minima tempora non ex velit dolores saepe ullam obcaecati optio dignissimos eveniet praesentium quo sapiente sed iste error autem aspernatur vel neque rem quos! Vitae quia numquam obcaecati esse blanditiis doloribus culpa cupiditate accusantium cum? Eius consectetur, a quia enim soluta incidunt, nemo dicta non rem recusandae quas ab deserunt beatae itaque laboriosam quibusdam labore? Ipsa hic dolorum quo ratione maxime ab, totam expedita dolores quasi quis blanditiis. Fuga, numquam nihil magni ipsam suscipit adipisci vitae optio molestias in accusantium possimus modi assumenda nemo iure magnam animi non, facilis voluptates, quisquam inventore. Deleniti ratione quidem earum repudiandae totam eos nulla laborum sequi ipsum dolor. Facilis accusamus iste qui inventore, aliquid dolor eius porro sapiente laudantium, quae exercitationem tempore labore? Quidem, cupiditate asperiores, similique alias soluta libero explicabo architecto ex minima quae voluptate nemo, ad blanditiis quia. Consequuntur, veniam.
+            </div>
+          </div>`,
+  'contact' : `<div class="card m-auto p-4 w-50 mt-4 bg-dark text-white border-white">
+                  <form>
+                      <div class="mb-3">
+                      <label for="name" class="form-label">Name</label>
+                      <input type="text" class="form-control" id="name" aria-describedby="emailHelp">
+                      
+                      </div>
+                      <div class="mb-3">
+                      <label for="number" class="form-label">Contact Number</label>
+                      <input type="number" class="form-control" id="number" aria-describedby="emailHelp">
+                      </div>
+                      <div class="mb-3">
+                      <label for="exampleInputEmail1" class="form-label">Email address</label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                      </div>
+                      
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                  </form>
+              </div>`,
+    'about' : `<div class="card m-auto w-75 mt-5 p-5 bg-dark text-white border-white">
+                <div class="card-header h3 bg-dark">
+                    About Us
+                </div>
+                <div class="card-body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim accusantium nam, exercitationem illo quae distinctio explicabo, repudiandae molestiae excepturi, officia quo nostrum qui possimus laudantium esse est molestias debitis iusto. Harum est, recusandae, sit porro corporis, error omnis quos ut fuga illo reprehenderit possimus nulla saepe ea at. Enim quibusdam dolor, deserunt, adipisci repudiandae necessitatibus id vel aut eaque numquam libero aperiam commodi qui amet, corporis accusantium quidem ut. In, ipsa quaerat soluta esse provident beatae hic non fugiat odit facere id obcaecati nesciunt dolores aperiam enim sunt ratione voluptatum? Et ratione iste error cum repudiandae, totam ipsam cupiditate ab iusto tempore commodi consectetur necessitatibus, autem eos hic architecto laudantium fugit iure? Optio impedit dicta doloremque atque quam molestiae dolorum libero ipsum maxime iusto ut vero recusandae, perspiciatis quibusdam corrupti magnam. Quisquam quidem molestias, possimus temporibus corrupti atque illo cumque odit ipsum porro impedit quis. Nam reprehenderit impedit eos amet ut eligendi enim repudiandae cum et? Deleniti possimus, cupiditate explicabo illum sapiente dolore. Aliquam officiis nihil, aliquid ipsam dicta quis, a aperiam nisi molestiae esse cumque laudantium deleniti itaque quos id voluptas iure, eligendi deserunt totam sequi cupiditate. Quasi in, nostrum illum, adipisci consectetur voluptatum quisquam id delectus debitis corporis expedita sequi tempore obcaecati dignissimos vero molestiae quia at quam dolor totam nisi assumenda esse non? Asperiores eum deleniti, a cum labore eveniet magnam voluptas doloribus vitae maiores itaque tenetur eligendi inventore qui aliquam at dolores consequatur quisquam commodi doloremque. Cupiditate error autem sint veritatis alias aut tenetur! Ut optio quisquam aspernatur similique minus quis modi neque praesentium explicabo vel rem atque, autem consectetur ipsam provident, esse voluptas eum omnis. Repellendus veritatis asperiores, rerum impedit provident debitis consequatur nam, sint aliquid mollitia facere rem eius saepe! Minima, porro illum. Odit, natus repellat hic quidem consectetur enim qui, laborum facilis debitis possimus quae iste a voluptas, eligendi illo. Placeat quaerat fugit non maiores mollitia autem numquam reiciendis alias, accusantium possimus itaque tempore minus molestiae sequi veniam quos perferendis et nihil quas eligendi dicta! Dolorem aut obcaecati quibusdam quia reiciendis odio, consectetur maxime a quidem aperiam corrupti repellat necessitatibus, itaque vero nesciunt dicta accusamus. Error necessitatibus sunt consectetur doloremque adipisci nemo obcaecati ad enim! Blanditiis ipsum aut quis eum amet obcaecati excepturi, nemo eveniet eos recusandae quas, atque at sed esse fugit. Aut tempore tenetur libero quos, rem sint soluta possimus consequuntur eos ipsum dolorum eveniet animi optio sunt commodi doloribus cupiditate!
+                </div>
+            </div>`,
+
+}
+
+
+let availableRoute = ['navbar', 'home', 'contact', 'about']
